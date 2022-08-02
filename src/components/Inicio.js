12c0a2930f946d790aef20c4d04d56769564fdf8
james <jameseliotv@gmail.com>
2022-08-01 19:52:52 -0500

    First commit

---
+++ src/components/Inicio.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Inicio = () => {
+    return (
+        <div>
+
+            <h1>
+
+            </h1>
+
+        </div>
+    )
+}
---
+++ src/components/layout/Fotter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Fotter = () => {
+    return (
+        <footer className='footer'>
+            Portafolio James Vargas WEB &copy; Front End DEV - 2022
+        </footer>
+    )
+}
