16c14fdf2248797bd180561b182f6beb9d76b9c5
james <jameseliotv@gmail.com>
2022-10-18 19:10:21 -0500

    add something 1

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -2,17 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Inicio = () => {
-    return (
-        <div className="home">
-            <h1>
-                Hey my name is <strong>James Vargas</strong>, I'm a
-                Frontend development with <strong>React JS</strong>, <strong>Angular</strong>,
-                <strong>Jquery programming languages</strong>, and <strong>CSS3</strong>, <strong>HTML5</strong>,
-                <strong>Bootstrap</strong> styling and tags.
-            </h1><br />
-            <h2>
-                <Link to="/contacto">Contact me.</Link>
-            </h2>
-        </div>
-    );
+  return (
+    <div className="home">
+      <h1>
+        Hello, my name is <strong> James Vargas</strong>, I'm a Frontend
+        developer, I work with programming languages like:
+        <strong> React JS</strong>, <strong> Angular</strong>,
+        <strong> Jquery</strong> and tag languages like: <strong> CSS3</strong>,
+        <strong> HTML5</strong>, <strong> Bootstrap</strong>. <br />
+        <br />
+        I am good at building interpersonal relationships and I always try to
+        have a good relationship with my team. <br />
+        <br />
+        Currently I have 1 year of experience, I define myself as a
+        <strong> responsible </strong>
+        and <strong>self-taught</strong> person, I like to listen to music, read
+        documentation on different technologies on the market and in my spare
+        time I like to exercise.
+      </h1>
+      <br />
+      <h2>
+        <Link to="/contacto">Contact me.</Link>
+      </h2>
+    </div>
+  );
 };
